07f37685174a807cf02bfd133102f251c04d875c
Elaidzha1940 <eli080293@gmail.com>
2024-03-30 23:10:14 +0300

    KeychainP

---
+++ KeychainP/ContentView.swift
@@ -0,0 +1,22 @@
+//  /*
+//
+//  Project: KeychainP
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 30.03.2024
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+         
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KeychainP/KeychainPApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: KeychainP
+//  File: KeychainPApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 30.03.2024
+//
+//  */
+
+import SwiftUI
+
+@main
+struct KeychainPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
