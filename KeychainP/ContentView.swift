5aa78db9e1411d083031eb355d97f71b298740ec
Elaidzha1940 <eli080293@gmail.com>
2024-03-31 14:58:44 +0300

    Update ContentView

--- KeychainP/ContentView.swift
+++ KeychainP/ContentView.swift
@@ -8,11 +8,27 @@
 //  */
 
 import SwiftUI
+import KeychainSwift
+
+// Keychain is similar to UserDefaults or AppStorage
+// Keychain is encrypted
+// Keychain is good for: passwords, ssn, credit card info, sensitive data
+// Keychain persists between installs and across devices
 
 struct ContentView: View {
+    let keychain = KeychainSwift()
+    @State private var userPassword: String = ""
+    
     var body: some View {
         VStack {
-         
+            Button(userPassword.isEmpty ? "No password" : userPassword) {
+                let newPassword = "eli0802"
+                keychain.set(newPassword, forKey: "user_password") // withAccess: .accessibleWhenUnlockedThisDeviceOnly
+                userPassword = newPassword
+            }
+            .onAppear {
+                userPassword = keychain.get("user_password") ?? ""
+            }
         }
     }
 }
