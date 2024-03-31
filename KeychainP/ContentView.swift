29cd3469387c546181c43db2202247151fe889d2
Elaidzha1940 <eli080293@gmail.com>
2024-03-31 15:33:14 +0300

    Last Update

--- KeychainP/ContentView.swift
+++ KeychainP/ContentView.swift
@@ -15,19 +15,55 @@ import KeychainSwift
 // Keychain is good for: passwords, ssn, credit card info, sensitive data
 // Keychain persists between installs and across devices
 
+final class KeychainManager {
+    private let keychain: KeychainSwift
+    
+    init() {
+        let keychain = KeychainSwift()
+        keychain.synchronizable = true
+        self.keychain = keychain
+    }
+    
+    func set(_ value: String, key: String) {
+        keychain.set(value, forKey: key)
+    }
+    
+    func get(key: String) -> String? {
+        keychain.get(key)
+    }
+}
+
+struct KeychainManagerKey: EnvironmentKey {
+    //
+    static let defaultValue: KeychainManager = KeychainManager()
+}
+
+extension EnvironmentValues {
+    var keychain: KeychainManager {
+        get { self[KeychainManagerKey.self] }
+        set { self[KeychainManagerKey.self] = newValue }
+    }
+}
+
 struct ContentView: View {
-    let keychain = KeychainSwift()
+    @Environment(\.keychain) var keychain
+    //let keychainManager = KeychainManager()
     @State private var userPassword: String = ""
     
     var body: some View {
         VStack {
             Button(userPassword.isEmpty ? "No password" : userPassword) {
                 let newPassword = "eli0802"
-                keychain.set(newPassword, forKey: "user_password") // withAccess: .accessibleWhenUnlockedThisDeviceOnly
-                userPassword = newPassword
+                keychain.set(newPassword, key: "user_password")
+                //                keychain.keychain.set(newPassword, forKey: "user_password") // withAccess: .accessibleWhenUnlockedThisDeviceOnly
+                //                userPassword = newPassword
+                
+                //                keychain.delete("my key")
+                //                keychain.clear()
             }
             .onAppear {
-                userPassword = keychain.get("user_password") ?? ""
+                userPassword = keychain.get(key: "user_password") ?? ""
+                //                userPassword = KeychainSwift().get("user_password") ?? ""
             }
         }
     }
